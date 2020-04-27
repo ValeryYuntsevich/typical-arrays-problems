c4687f0043f59c36064350bed18411348a4cf0ad
valery <valery.yuntsevich@yandex.ru>
2020-04-27 17:42:10 +0300

    release v1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  let isArray = array ? array : [];
+ 
+  if(isArray.length === 0) {
+    return 0
+  }
+
+  if(isArray.length > 0) {
+    let min = Math.min(...isArray);
+    return min
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let isArray = array ? array : [];
+ 
+  if(isArray.length === 0) {
+    return 0
+  }
+
+  if(isArray.length > 0) {
+    let max = Math.max(...isArray);
+    return max
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let isArray = array ? array : [];
+ 
+  if(isArray.length === 0) {
+    return 0
+  }
+
+  if(isArray.length > 0) {
+    let avarage = isArray.reduce((a,b) => a + b, 0) / isArray.length;
+    return avarage
+  }
 }
